0794bec20ae8fc9aef177ad2aba1a942204f3eeb
Juan Esteban Carrillo Garcia <ju4nc4rrill00@gmail.com>
2023-05-02 11:40:39 -0500

    Botones Colores, Borrar

--- src/Vistas/Canvas.tsx
+++ src/Vistas/Canvas.tsx
@@ -3,6 +3,9 @@ import React, { useRef, useState } from 'react';
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [isDrawing, setIsDrawing] = useState<boolean>(false);
+  const [color, setColor] = useState<string>('black');
+  const [isErasing, setIsErasing] = useState<boolean>(false);
+  const [strokes, setStrokes] = useState<Array<Array<number>>>([]);
 
   const startDrawing = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const canvas = canvasRef.current;
@@ -15,13 +18,24 @@ function Canvas() {
   };
 
   const continueDrawing = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    if (isDrawing) {
-      const canvas = canvasRef.current;
-      const context = canvas?.getContext('2d');
-      if (!context) return;
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d');
+    if (!context) return;
+
+    if (isErasing) {
+      // Borrar área de 3px alrededor del cursor
+      if (canvas) {
+        const { left, top } = canvas.getBoundingClientRect();
+        const { clientX, clientY } = event;
+        const x = clientX - left;
+        const y = clientY - top;
+        context.clearRect(x - 3, y - 3, 30, 30);
+      }
+    } else if (isDrawing) {
+      // Dibujar trazo
       const { offsetX, offsetY } = event.nativeEvent;
       context.lineTo(offsetX, offsetY);
-      context.strokeStyle = 'blue';
+      context.strokeStyle = color;
       context.stroke();
     }
   };
@@ -31,31 +45,71 @@ function Canvas() {
   };
 
   const addHoverClass = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isErasing) {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+      const context = canvas.getContext('2d');
+      if (!context) return;
+      const { left, top } = canvas.getBoundingClientRect();
+      const { clientX, clientY } = event;
+      const x = clientX - left;
+      const y = clientY - top;
+
+      context.beginPath();
+      context.arc(x, y, 20, 0, 2 * Math.PI);
+      context.fillStyle = 'white';
+      context.fill();
+    }
+  };
+
+  const removeHoverClass = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const canvas = canvasRef.current;
     if (!canvas) return;
     const context = canvas.getContext('2d');
     if (!context) return;
-    const { left, top } = canvas.getBoundingClientRect();
-    const { clientX, clientY } = event;
-    const x = clientX - left;
-    const y = clientY - top;
-    context.beginPath();
-    context.fill();
+    context.clearRect(0, 0, canvas.width, canvas.height);
   };
 
+  const clearCanvas = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  };
 
+  const borrar = () => {
+    if(isErasing){
+      setIsErasing(false);
+    }else{
+      setIsErasing(true);
+    }
+  };
+  
   return (
-    <canvas
-      ref={canvasRef}
-      width={1200} // Tamaño suficiente para dibujar
-      height={500}
-      onMouseDown={startDrawing}
-      onMouseMove={continueDrawing}
-      onMouseUp={stopDrawing}
-      onMouseEnter={addHoverClass}
-      style={{border: '1px solid black'}}
-    />
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={500} // Tamaño suficiente para dibujar
+        height={500}
+        onMouseDown={startDrawing}
+        onMouseMove={continueDrawing}
+        onMouseUp={stopDrawing}
+        onMouseEnter={addHoverClass}
+        style={{ border: '1px solid black' }}
+      />
+      <div>
+        <button onClick={() => setColor('red')}>Rojo</button>
+        <button onClick={() => setColor('blue')}>Azul</button>
+        <button onClick={() => setColor('green')}>Verde</button>
+        <button onClick={() => setColor('black')}>Negro</button>
+        <button onClick={() => clearCanvas()}>Borrar Todo</button>
+        <button onClick={() => borrar()}>Borrar</button>
+      </div>
+    </div>
   );
 }
 
 export default Canvas;
+
+
